3b95b26ae9c3a1d24c39e8e0ca3e91921eb65550
trantrongbinh <tranbinhbak@gmail.com>
2018-06-08 17:14:25 +0700

    Web Xem Phim Online

---
+++ Movies/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    public class AdminController : Controller
+    {
+        QuanLyMovieEntities db = new QuanLyMovieEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult TrangChuAdminPartial()
+        {
+            return PartialView();
+        }
+
+        //GET theloai
+        public ViewResult TheLoai()
+        {
+            return View(db.theloais.ToList());
+        }
+
+        //GET ThemTL
+        public ViewResult ThemTL()
+        {
+            return View();
+        }
+    }
+}
---
+++ Movies/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+
+        public ViewResult DanhSachTheLoai()
+        {
+            return View();
+        }
+
+        public ViewResult DanhSachQuocGia()
+        {
+            return View();
+        }
+
+        public ViewResult ChiTiet()
+        {
+            return View();
+        }
+    }
+}
